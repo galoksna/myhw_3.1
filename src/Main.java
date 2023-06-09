b29dec086c8979ae229403415a7dc711b9a1651e
Oksana Gal <oksana.gal@list.ru>
2023-06-09 21:32:52 +0300

    file changed2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int a = 1800;
-        int b = 20;
-        int ticketPrise = a;
-        int milePrise = b;
+        int ticketPrise = 1800;
+        int milePrise = 20;
         int bonus = ticketPrise / milePrise;
 
         System.out.println(bonus);
