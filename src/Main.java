3a6154e6252ba7cdc6c11a3d6d71b3c3fae1b6b9
Oksana Gal <oksana.gal@list.ru>
2023-06-09 16:54:43 +0300

    giles added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 54678;
+        int ticketPrise = x;
+        int oneMilePrise = 20;
+        int bonusMile = x / oneMilePrise;
+        
+        System.out.println(bonusMile);
+
+    }
+}
